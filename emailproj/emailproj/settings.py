fd237662a7e419414f9a7bdade9e58214c75598b
Shibu <nestsoftworks@gmail.com>
2019-10-31 20:17:58 +0530

    sdf

--- emailproj/emailproj/settings.py
+++ emailproj/emailproj/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from emailproj.emailproj.celery import app
+from .celery import app
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
